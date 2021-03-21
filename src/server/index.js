3e4fd63f2216020817b2eeba895cff10b03e7d35
Mark Guerrero <mark2579@gmail.com>
2021-03-21 00:50:32 -0400

    feat: Added eslint and styles and update server start

---
+++ src/server/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const port = process.env.PORT || 8090;
+
+const app = express();
+const router = express.Router();
+
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('dist'));
+
+router.get('/', (req, res) => {
+  res.send('Ok');
+});
+
+app.listen(port, () => {
+  console.log('Welcome to Travel Plano');
+  console.log('A Travel Application to pull multiple types of data from different sources. A Udacity project presented by Mark Guerrero');
+  console.log(`Application is running on port ${port}`);
+});
