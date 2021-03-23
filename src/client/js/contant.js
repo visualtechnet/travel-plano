b7ab7d79e7650f1485a908ab625ac5812422de70
Mark Guerrero <mark2579@gmail.com>
2021-03-23 02:20:09 -0400

    fix: updated travel server url

--- src/client/js/contant.js
+++ src/client/js/contant.js
@@ -1,4 +1,4 @@
 export const MYTRIPS = 'MYTRIPS';
 export const RECENTTRIPS = 'RECENTTRIPS';
 export const DATEFORMAT = 'YYYY-MM-DD';
-export const TRAVEL_SERVER_URL = 'http://localhost:8090';
+export const TRAVEL_SERVER_URL = 'https://travel-plano-server.herokuapp.com/';
